10e419bb1c9c5fd8f90f885e10cda17d4100ca26
Frakist <90983918+Frakist@users.noreply.github.com>
2023-09-23 16:24:42 +0300

    update

--- index.js
+++ index.js
@@ -17,46 +17,43 @@ client.login(token);
 client.on(Events.MessageCreate, async message => {
     if (message.channelId != 1154750800904593418) return;
     if (!message.author.bot) {
-        if (message.content.includes("clear data") && message.author.id == "464025143790927872") {
-            db = {};
-            message.reply("data has been resetted");
-            return;
-        }
-        if (message.content.includes("show db") && message.author.id == "464025143790927872") {
-            message.reply(JSON.stringify(db, null, 4));
-            return;
-        }
-        if (message.content.includes("how much xp left")) {
-            message.reply((Math.round(100 * (db[message.author.id].lv * 0.043) * 100) / 100).toString());
-            return;
-        }
+        switch (message.content.toLowerCase()) {
+			case 'how much xp left':
+				message.reply((Math.round(100 * (db[message.author.id].lv * 0.043) * 100) / 100 - db[message.author.id].xp).toString());
+				return;
+		}
         let tsMultiplier;
         if (db[message.author.id] == null) {
             db[message.author.id] = { xp: 1, ts: message.createdTimestamp / 5, lv: 1, xpTotal: 1 };
             message.reply(`db account created for ${message.author.id}`);
+            return
         }
-        else {
-            let dbuser = db[message.author.id];
-            tsMultiplier = +(Math.round((((message.createdTimestamp / 5) - dbuser.ts) / 8200) + "e+2") + "e-2"); //time between messages
-            tsMultiplier += Math.max(Math.min((10000 - ((message.createdTimestamp / 5) - dbuser.ts)) / 2000, 0.01), -(tsMultiplier)); //penalize for taking too long to send messages
-            if (message.content.length != 0) {
-                tsMultiplier += ((findUnique(message.content).length) - message.content.length / 10) / 40; //unique characters minus the amount of total characters
-                if (emotes(message.content) != null) tsMultiplier /= emotes(message.content).length + 1;
-                if (message.content.includes('http')) tsMultiplier /= message.content.length;
-            }
-            console.log(message.content);
-            dbuser.xpTotal = Math.round((dbuser.xpTotal + 1 * tsMultiplier) * 100) / 100;
-            dbuser.xp = Math.round((dbuser.xp + 1 * tsMultiplier) * 100) / 100;
-            message.reply(`hi ur total xp is ${dbuser.xp}xp with the increase of ${(Math.round(tsMultiplier * 100) / 100)}xp (ur total lifetime xp is ${dbuser.xpTotal}`);
-
-            dbuser.ts = message.createdTimestamp / 5;
-
-            if (dbuser.xp > 100 * (dbuser.lv * 0.043)) {
-                dbuser.xp = 0;
-                dbuser.lv += 1;
-                message.reply(`also u just reached level ${dbuser.lv} and need to reach ${Math.round(100 * (dbuser.lv * 0.043) * 100) / 100} for the next level`);
-            }
-        }
+        switch (message.content.toLowerCase()) {
+			case 'how much xp left':
+				message.reply((Math.round(100 * (db[message.author.id].lv * 0.043) * 100) / 100 - db[message.author.id].xp).toString());
+				break;
+            default:
+                let dbuser = db[message.author.id];
+                tsMultiplier = +(Math.round((((message.createdTimestamp / 5) - dbuser.ts) / 8200) + "e+2") + "e-2"); //time between messages
+                tsMultiplier += Math.max(Math.min((10000 - ((message.createdTimestamp / 5) - dbuser.ts)) / 2000, 0.01), -(tsMultiplier)); //penalize for taking too long to send messages
+                if (message.content.length != 0) {
+                    tsMultiplier += ((findUnique(message.content).length) - message.content.length / 10) / 40; //unique characters minus the amount of total characters
+                    if (emotes(message.content) != null) tsMultiplier /= emotes(message.content).length + 1;
+                    if (message.content.includes('http')) tsMultiplier /= message.content.length;
+                }
+                console.log(message.content);
+                dbuser.xpTotal = Math.round((dbuser.xpTotal + 1 * tsMultiplier) * 100) / 100;
+                dbuser.xp = Math.round((dbuser.xp + 1 * tsMultiplier) * 100) / 100;
+                message.reply(`hi ur total xp is ${dbuser.xp}xp with the increase of ${(Math.round(tsMultiplier * 100) / 100)}xp (ur total lifetime xp is ${dbuser.xpTotal}`);
+    
+                dbuser.ts = message.createdTimestamp / 5;
+    
+                if (dbuser.xp > 100 * (dbuser.lv * 0.043)) {
+                    dbuser.xp = 0;
+                    dbuser.lv += 1;
+                    message.reply(`also u just reached level ${dbuser.lv} and need to reach ${Math.round(100 * (dbuser.lv * 0.043) * 100) / 100} for the next level`);
+                }
+		}
     }
 });
 const emotes = (str) => str.match(/<a?:.+?:\d{18}>|\p{Extended_Pictographic}/gu);
